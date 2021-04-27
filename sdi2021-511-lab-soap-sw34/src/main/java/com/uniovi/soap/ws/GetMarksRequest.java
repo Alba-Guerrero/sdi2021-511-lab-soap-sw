a07b1889ecaed9561a704a10cd44868d6aa0e0b7
Alba <uo266007@uniovi.es>
2021-04-27 09:50:11 +0200

    SDI-2021-511-11.2-Servicio SOAP Cliente

--- sdi2021-511-lab-soap-sw34/src/main/java/com/uniovi/soap/ws/GetMarksRequest.java
+++ sdi2021-511-lab-soap-sw34/src/main/java/com/uniovi/soap/ws/GetMarksRequest.java
@@ -1,12 +1,12 @@
 //
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.04.27 a las 09:49:13 AM CEST 
+// Generado el: 2021.04.26 a las 10:45:55 AM CEST 
 //
 
 
-package com.uniovi.wsdl;
+package com.uniovi.soap.ws;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -25,7 +25,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent&gt;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
  *       &lt;sequence&gt;
- *         &lt;element name="user" type="{http://uniovi.com/soap/ws}user"/&gt;
+ *         &lt;element name="dni" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
  *       &lt;/sequence&gt;
  *     &lt;/restriction&gt;
  *   &lt;/complexContent&gt;
@@ -36,36 +36,36 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "user"
+    "dni"
 })
-@XmlRootElement(name = "getMarksResponse")
-public class GetMarksResponse {
+@XmlRootElement(name = "getMarksRequest")
+public class GetMarksRequest {
 
     @XmlElement(required = true)
-    protected User user;
+    protected String dni;
 
     /**
-     * Obtiene el valor de la propiedad user.
+     * Obtiene el valor de la propiedad dni.
      * 
      * @return
      *     possible object is
-     *     {@link User }
+     *     {@link String }
      *     
      */
-    public User getUser() {
-        return user;
+    public String getDni() {
+        return dni;
     }
 
     /**
-     * Define el valor de la propiedad user.
+     * Define el valor de la propiedad dni.
      * 
      * @param value
      *     allowed object is
-     *     {@link User }
+     *     {@link String }
      *     
      */
-    public void setUser(User value) {
-        this.user = value;
+    public void setDni(String value) {
+        this.dni = value;
     }
 
 }
--- sdi2021-511-lab-soap-sw34/src/main/java/com/uniovi/soap/ws/ObjectFactory.java
+++ sdi2021-511-lab-soap-sw34/src/main/java/com/uniovi/soap/ws/ObjectFactory.java
@@ -1,12 +1,12 @@
 //
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.04.27 a las 09:49:13 AM CEST 
+// Generado el: 2021.04.26 a las 10:45:55 AM CEST 
 //
 
 
-package com.uniovi.wsdl;
+package com.uniovi.soap.ws;
 
 import javax.xml.bind.annotation.XmlRegistry;
 
@@ -14,7 +14,7 @@ import javax.xml.bind.annotation.XmlRegistry;
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the com.uniovi.wsdl package. 
+ * generated in the com.uniovi.soap.ws package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -30,7 +30,7 @@ public class ObjectFactory {
 
 
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.uniovi.wsdl
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.uniovi.soap.ws
      * 
      */
     public ObjectFactory() {
